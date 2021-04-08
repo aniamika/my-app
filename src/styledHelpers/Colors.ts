664f7aaae4650ddb47d35fbdc8a77362b9e7ea4b
Anna Mika <aniamika90@gmail.com>
2021-04-08 21:13:47 +0200

    left sidebar styling

--- src/styledHelpers/Colors.ts
+++ src/styledHelpers/Colors.ts
@@ -1,7 +1,11 @@
 export const Colors = {
     white: '#FFF',
     black: '#000',
-    gray: '#F5F7F9',
-    grayMiddle: '#ACAFB3',
-    darkGray: '#777'
+
+    gray01: '#F5F7F9',
+    gray02: '#e8e8e8',
+    gray03: '#ACAFB3',
+    
+    blue01: '#2a3f9d',
+    blue02: '#232c47',
 }
