3f1b33f67e2b0aaba1b158fa6bce2fc2d0f6326a
Anna Mika <aniamika90@gmail.com>
2021-06-09 22:01:51 +0200

    profile page styling

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -1,6 +1,14 @@
-import React, { FC } from "react";
+import React from "react";
+import { useSelector } from "react-redux";
 import styled from "styled-components";
 import IconButtonGeneric from "../../components/Common/IconButtonGeneric";
+import { IState } from "../../reducers";
+import { IAlbumsReducer } from "../../reducers/albumsReducers";
+import { ICommentsReducer } from "../../reducers/commentsReducers";
+import { IPhotosReducer } from "../../reducers/photosReducers";
+import { IPostsReducer } from "../../reducers/postsReducers";
+import { ITodosReducer } from "../../reducers/todosReducers";
+import { IUsersReducer } from "../../reducers/usersReducers";
 import { Colors } from "../../styledHelpers/Colors";
 import { FontSize } from "../../styledHelpers/FontSize";
 import { Margins } from "../../styledHelpers/Margins";
@@ -36,15 +44,66 @@ const HeaderMain = styled.div`
   display: flex;
   justify-content: space-between;
 `
+const PersonContainer = styled.div`
+  border: 1px solid red;
+`
 const ImageContainer = styled.div`
-  background: pink;
   display: flex;
   align-items: center;
+  width: 80px;
+  height: 80px;
+  border-radius: 50%;
+  margin-bottom: ${Margins[8]};
+  position: relative;
+
+  &:after {
+    content: "*";
+    display: block;
+    border-radius: 16px;
+    background-color: #0381BE;
+    z-index: 1;
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    height: 1.5rem;
+    width: 1.5rem;
+    color: ${Colors.white};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: 2px solid ${Colors.white};
+    line-height: normal;
+  }
+
+`
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  overflow: hidden;
+  border-radius: 50%;
+`;
+const SeeProfileButton = styled.button`
+  color: ${Colors.blue03};
+  font-size: ${FontSize[16]};
+  font-weight: 500;
 `
 const PersonInfo = styled.div`
   background: lightblue;
   display: flex;
-  align-items: center;
+  flex-direction: column;
+`
+const PersonName = styled.p`
+
+`
+const PersonCompany = styled.div`
+
+`
+const PersonCity = styled.div`
+
+`
+const PersonRole = styled.div`
+
 `
 const PersonDetails = styled.div`
   background: lightgreen;
@@ -60,7 +119,17 @@ const SectionHeader = styled.p`
   margin-bottom: ${Margins[16]};
 `
 
-export const ProfilePage: FC = () => {
+export const ProfilePage = () => {
+
+  const { albumsList, commentsList, photosList, postsList, todosList, usersList } = useSelector<IState, IAlbumsReducer & ICommentsReducer & IPhotosReducer & IPostsReducer & ITodosReducer & IUsersReducer >(state => ({
+    ...state.albums,
+    ...state.comments,
+    ...state.photos,
+    ...state.posts,
+    ...state.todos,
+    ...state.users,
+  }));
+  
   return (
     <Wrapper>
       <Header> 
@@ -79,8 +148,18 @@ export const ProfilePage: FC = () => {
           </HeaderButton>
         </HeaderButtonsContainer>
         <HeaderMain>
-          <ImageContainer> Image </ImageContainer>
-          <PersonInfo> Info </PersonInfo>
+          <PersonContainer>
+            <ImageContainer>
+                <Image src={photosList[0]?.url} />
+            </ImageContainer>
+            <SeeProfileButton>See profile</SeeProfileButton>
+          </PersonContainer>
+          <PersonInfo> 
+            <PersonName>Humberta swift</PersonName>
+            <PersonCompany>Clifford Chance</PersonCompany>
+            <PersonCity>New York</PersonCity>
+            <PersonRole>Partner</PersonRole>
+          </PersonInfo>
           <PersonDetails> Details </PersonDetails>
         </HeaderMain>
       </Header>
